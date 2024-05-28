644d82e37ac3cc7ababafeb02514ea482f39075e
Haydencreates <emmasaprofessional@gmail.com>
2024-05-28 15:17:13 -0500

    modified:   src/scenes/footer/index.tsx

--- src/scenes/footer/index.tsx
+++ src/scenes/footer/index.tsx
@@ -1,7 +1,6 @@
 import Logo from "@/assets/Logo.png"
-type Props = {}
 
-const Footer = (props: Props) => {
+const Footer = () => {
   return <footer className="bg-primary-100 py-16">
     <div className="justify-content mx-auto w-5/6 gap-16 md:flex">
         <div className="mt-16 basis-1/2 md:mt-0">
